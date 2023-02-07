47bddfff7ca7ac3a039eaeb1727621c49326e972
IraLysenko <arch_ira@ukr.net>
2023-02-07 11:45:46 +0100

    chore(heroes): added hero detail component, message, hero service

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "../../types/hero";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { Observable, of } from 'rxjs';
+import {HEROES} from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
